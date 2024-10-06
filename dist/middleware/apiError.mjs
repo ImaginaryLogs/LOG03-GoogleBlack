2732cd763de97f5e2082d67713c98e2199450a19
Logyre <logarithmicpresence@gmail.com>
2024-10-06 13:43:06 +0800

    Reworked some server files.

--- dist/middleware/apiError.mjs
+++
@@ -1,9 +0,0 @@
-export class apiError extends Error {
-    constructor(name, message, stacktrace = "") {
-        super(message);
-        this.message = message;
-        this.name = name;
-        this.stack = stacktrace;
-    }
-}
-//# sourceMappingURL=apiError.mjs.map
